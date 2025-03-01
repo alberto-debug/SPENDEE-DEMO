a6931cb63c465f099956d7c064de81bdb58c5797
Alberto <albertojunior848423803@gmail.com>
2025-03-01 15:05:33 +0300

    changes

--- src/main/java/com/example/loginauthapi/repositories/TaskRepository.java
+++ src/main/java/com/example/loginauthapi/repositories/TaskRepository.java
@@ -1,9 +1,14 @@
 package com.example.loginauthapi.repositories;
 
 import com.example.loginauthapi.domain.user.Task;
+import com.example.loginauthapi.domain.user.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task,Long> {
+    List<Task> findByUserId(Long userId);
+    List<Task> findByStatus(TaskStatus status);
 }
