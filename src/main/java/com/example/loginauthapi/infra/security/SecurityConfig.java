215f411733909afa35528742771ac502dfd32081
Alberto <albertojunior848423803@gmail.com>
2025-05-04 00:21:41 +0300

    updates

--- src/main/java/com/example/loginauthapi/infra/security/SecurityConfig.java
+++ src/main/java/com/example/loginauthapi/infra/security/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig {
         return authProvider;
     }
 
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
