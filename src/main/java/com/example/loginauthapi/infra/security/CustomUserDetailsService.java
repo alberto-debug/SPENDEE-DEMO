1981d1ec982302f1ce83508d53d9f27577a58e2e
Alberto <albertojunior848423803@gmail.com>
2025-05-04 02:11:55 +0300

    updates

--- src/main/java/com/example/loginauthapi/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/example/loginauthapi/infra/security/CustomUserDetailsService.java
@@ -3,7 +3,9 @@ package com.example.loginauthapi.infra.security;
 import com.example.loginauthapi.domain.user.User;
 import com.example.loginauthapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
@@ -17,7 +19,15 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found for email: " + username));
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName()))
+                        .toList()
+        );
+
     }
 
 }
