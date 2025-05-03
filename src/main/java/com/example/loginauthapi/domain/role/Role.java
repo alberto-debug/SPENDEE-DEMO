e231cb6f7b8030656de4290ec08471cf8ab0d33f
Alberto <albertojunior848423803@gmail.com>
2025-05-04 01:22:56 +0300

    updates

--- src/main/java/com/example/loginauthapi/domain/role/Role.java
+++ src/main/java/com/example/loginauthapi/domain/role/Role.java
@@ -1,17 +1,19 @@
 package com.example.loginauthapi.domain.role;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.loginauthapi.domain.user.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @Setter
+@Table(name = "roles")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Role {
@@ -19,4 +21,11 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    //states that roles and users have a relationship and
+    //Automatically links roles back to their users via user_roles table.
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users =  new HashSet<>();
 }
