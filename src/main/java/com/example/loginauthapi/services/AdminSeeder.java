552be8cfb314a6f5ccbc0c675343c2cbb8ab40b2
Alberto <albertojunior848423803@gmail.com>
2025-05-09 23:56:25 +0300

    updates

--- src/main/java/com/example/loginauthapi/services/AdminSeeder.java
+++ src/main/java/com/example/loginauthapi/services/AdminSeeder.java
@@ -5,11 +5,12 @@ import com.example.loginauthapi.domain.user.User;
 import com.example.loginauthapi.repositories.RoleRepository;
 import com.example.loginauthapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AdminSeeder {
+public class AdminSeeder implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
@@ -20,25 +21,26 @@ public class AdminSeeder {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+
     @Override
     public void run(String... args) throws Exception {
 
-        User admin = null;
-        if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
-            Role dminRole = roleRepository.findByName("ROLE_ADMIN")
-                    .orElseThrow(() -> new RuntimeException("ROLE_ADMIN not found"));
+            User admin = null;
+            if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
+                Role dminRole = roleRepository.findByName("ROLE_ADMIN")
+                        .orElseThrow(() -> new RuntimeException("ROLE_ADMIN not found"));
 
-            admin = new User();
-            admin.setName("Admin");
-            admin.setEmail("${adminEmail}");
-            admin.setPassword(passwordEncoder.encode("${adminPassword}"));
-            admin.getRoles().add(dminRole);
+                admin = new User();
+                admin.setName("Admin");
+                admin.setEmail("${adminEmail}");
+                admin.setPassword(passwordEncoder.encode("${adminPassword}"));
+                admin.getRoles().add(dminRole);
 
-            userRepository.save(admin);
+                userRepository.save(admin);
 
-            System.out.println("✅ Admin user created");
-        }
-
+                System.out.println("✅ Admin user created");
+            }
+            
 
     }
 }
