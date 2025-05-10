da1b4fc98e68b333a410f040ef9c05565a26a945
Alberto <albertojunior848423803@gmail.com>
2025-05-10 23:11:45 +0300

    updates

--- src/main/java/com/example/loginauthapi/services/AdminSeeder.java
+++ src/main/java/com/example/loginauthapi/services/AdminSeeder.java
@@ -4,7 +4,9 @@ import com.example.loginauthapi.domain.role.Role;
 import com.example.loginauthapi.domain.user.User;
 import com.example.loginauthapi.repositories.RoleRepository;
 import com.example.loginauthapi.repositories.UserRepository;
+import org.hibernate.annotations.AnyDiscriminatorValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -21,17 +23,23 @@ public class AdminSeeder implements CommandLineRunner {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Value("${admEmail}")
+    private String adminEmail;
+
+    @Value("${admPassword}")
+    private String adminPassword;
+
     @Override
     public void run(String... args) throws Exception {
 
-        if (userRepository.findByEmail("${AdminEmail}").isEmpty()){
+        if (userRepository.findByEmail(adminEmail).isEmpty()){
             Role adminRole = roleRepository.findByName("ROLE_ADMIN")
                     .orElseThrow(()-> new RuntimeException("Role Admin not found"));
 
             User admin = new User();
             admin.setName("Admin");
-            admin.setEmail("${adminEmail}");
-            admin.setPassword(passwordEncoder.encode("${AdminPassword}"));
+            admin.setEmail(adminEmail);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
             admin.getRoles().add(adminRole);
 
             userRepository.save(admin);
