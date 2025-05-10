8e62862ec22162b261516d4dc50898f92e415438
Alberto <albertojunior848423803@gmail.com>
2025-05-11 02:19:07 +0300

    updates

--- src/main/java/com/example/loginauthapi/controllers/AdminAuthController.java
+++ src/main/java/com/example/loginauthapi/controllers/AdminAuthController.java
@@ -1,8 +1,33 @@
 package com.example.loginauthapi.controllers;
 
+import com.example.loginauthapi.dto.LoginRequestDTO;
+import com.example.loginauthapi.repositories.RoleRepository;
+import com.example.loginauthapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin")
 public class AdminAuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/login")
+    public ResponseEntity<?> adminLogin(@RequestBody LoginRequestDTO body){
+
+        return null;
+    }
 }
