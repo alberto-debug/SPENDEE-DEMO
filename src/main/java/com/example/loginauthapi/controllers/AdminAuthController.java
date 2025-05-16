1fcf1bd743c46640c3d2a3ff76bcf1411bd34995
Alberto <albertojunior848423803@gmail.com>
2025-05-16 23:00:13 +0300

    updates

--- src/main/java/com/example/loginauthapi/controllers/AdminAuthController.java
+++ src/main/java/com/example/loginauthapi/controllers/AdminAuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/admin")
 public class AdminAuthController {
-    
+
     @Autowired
     private UserRepository userRepository;
 
