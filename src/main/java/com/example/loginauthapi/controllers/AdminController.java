8286950d9c9e0f9bdaa3d6139d2eb19470e4476c
Alberto <albertojunior848423803@gmail.com>
2025-05-16 23:38:46 +0300

    updates

--- src/main/java/com/example/loginauthapi/controllers/AdminController.java
+++ src/main/java/com/example/loginauthapi/controllers/AdminController.java
@@ -1,13 +1,12 @@
 package com.example.loginauthapi.controllers;
 
 
+import com.example.loginauthapi.domain.user.Transaction;
 import com.example.loginauthapi.domain.user.User;
+import com.example.loginauthapi.dto.UserSummary;
 import com.example.loginauthapi.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +18,12 @@ public class AdminController {
     private AdminService adminService;
 
     @GetMapping("/users")
-    public List<User> getAllUsers(@RequestBody User user){
-        return adminService.getAllUsers(user);
+    public List<UserSummary> getAllUsers(){
+        return adminService.getAllUsers();
+    }
+
+    @GetMapping("/users/{email}/transactions")
+    public List<Transaction> getUserTransactions(@PathVariable String email){
+        return  adminService.getUserTransactions()
     }
 }
