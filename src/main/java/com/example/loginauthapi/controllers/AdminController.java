e979c3dffdd95e77d6e8a586e18917d4d4415fa5
Alberto <albertojunior848423803@gmail.com>
2025-05-16 23:28:28 +0300

    updates

--- src/main/java/com/example/loginauthapi/controllers/AdminController.java
+++ src/main/java/com/example/loginauthapi/controllers/AdminController.java
@@ -1,8 +1,25 @@
 package com.example.loginauthapi.controllers;
 
 
+import com.example.loginauthapi.domain.user.User;
+import com.example.loginauthapi.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(@RequestBody User user){
+        return adminService.getAllUsers(user);
+    }
 }
