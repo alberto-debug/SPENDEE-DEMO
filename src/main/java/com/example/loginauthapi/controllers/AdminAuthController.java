d6305062ab6a9f5ba4b23f86d8e0aa0d1c22b4e1
Alberto <albertojunior848423803@gmail.com>
2025-05-16 23:00:28 +0300

    updates

--- src/main/java/com/example/loginauthapi/controllers/AdminAuthController.java
+++ src/main/java/com/example/loginauthapi/controllers/AdminAuthController.java
@@ -9,7 +9,6 @@ import com.example.loginauthapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
